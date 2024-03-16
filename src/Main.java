bd4ea35cebc9ddf6bbd5ed34be7528a778262724
AdamKudr <kudr.adam@gmail.com>
2024-03-16 20:24:25 +0100

    Doplněny metody do restaurant manager - price per table, print price per table.

--- src/Main.java
+++ src/Main.java
@@ -1,47 +1,18 @@
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         Cookbook menu = new Cookbook();
         Orders orders = new Orders();
-        RestaurantManager manager = new RestaurantManager(menu, orders);
+        RestaurantManager manager = new RestaurantManager(orders);
 
-        try {
-            menu.addDish(new Dish(1, "Ragu", BigDecimal.valueOf(125.50), 35, "ragu_01"));
-        } catch (OrdersException e) {
-            System.err.println("Nastala chyba při vytváření jídla:" + e.getLocalizedMessage());
-        }
-
-        try {
-            menu.addDish(new Dish(2, "Spagety", BigDecimal.valueOf(160), 40));
-        } catch (OrdersException e) {
-            System.err.println("Nastala chyba při vytváření jídla:" + e.getLocalizedMessage());
-        }
-
-        orders.addOrder(new Orders(1, 3, menu.cookbook.get(0), LocalDateTime.now()));
-        orders.addOrder(new Orders(4, 2, menu.cookbook.get(1), LocalDateTime.of(2024, 3, 12, 16, 20)));
-        orders.addOrder(new Orders(4, 1, menu.cookbook.get(0), LocalDateTime.of(2024, 3, 12, 16, 50)));
-        try {
-            menu.saveToFile("menu.txt");
-        } catch (OrdersException e) {
-            System.err.println("Nastala chyba při ukládání do souboru " + e.getLocalizedMessage());
-        }
-
-        System.out.println(menu.getCookbook());
-
-        try {
-            orders.saveToFile("orders.txt");
-        } catch (OrdersException e) {
-            System.err.println("Nastala chyba při ukládání do souboru " + e.getLocalizedMessage());
-        }
-
+        //načtení dat - úkol č. 1
         try {
             menu.loadFromFile("menu.txt");
         } catch (OrdersException e) {
-                System.err.println("Nastala chyba při načtení souboru! " + e.getLocalizedMessage());
-            }
+            System.err.println("Nastala chyba při načtení souboru! " + e.getLocalizedMessage());
+        }
 
         try {
             orders.loadFromFile("orders.txt", menu);
@@ -49,11 +20,72 @@ public class Main {
             System.err.println("Nastala chyba při načtení souboru! " + e.getLocalizedMessage());
         }
 
+        // vložení jídel a objednávek do systému - úkol č. 2
+        try {
+            menu.addDish(new Dish(1, "Kuřecí řízek obalovaný 150g", BigDecimal.valueOf(125.50), 35, "Kureci_rizek_150"));
+        } catch (OrdersException e) {
+            System.err.println("Nastala chyba při vytváření jídla:" + e.getLocalizedMessage());
+        }
+
+        try {
+            menu.addDish(new Dish(2, "Hranolky 150g", BigDecimal.valueOf(75), 10, "Hranolky_150"));
+        } catch (OrdersException e) {
+            System.err.println("Nastala chyba při vytváření jídla:" + e.getLocalizedMessage());
+        }
+
+        try {
+            menu.addDish(new Dish(3, "Pstruh na víně 200g", BigDecimal.valueOf(255), 45, "Pstruh_200"));
+        } catch (OrdersException e) {
+            System.err.println("Nastala chyba při vytváření jídla:" + e.getLocalizedMessage());
+        }
+
+        try {
+            menu.addDish(new Dish(4, "Kofola 0,5l", BigDecimal.valueOf(39), 4, "Kofola_05"));
+        } catch (OrdersException e) {
+            System.err.println("Nastala chyba při vytváření jídla:" + e.getLocalizedMessage());
+        }
+
+        orders.addOrder(new Orders(15, 2, menu.cookbook.get(0), LocalDateTime.of(2024, 3, 16, 17, 15)));
+        orders.addOrder(new Orders(15, 2, menu.cookbook.get(1), LocalDateTime.of(2024, 3, 16, 17, 16)));
+        orders.addOrder(new Orders(15, 2, menu.cookbook.get(3), LocalDateTime.of(2024, 3, 16, 17, 17)));
+        orders.orderFulfilled(orders.orders.get(2));
+
+        orders.addOrder(new Orders(2, 3, menu.cookbook.get(1), LocalDateTime.of(2024, 3, 16, 17, 22)));
+        orders.addOrder(new Orders(2, 3, menu.cookbook.get(3), LocalDateTime.of(2024, 3, 16, 17, 23)));
+
+        //celková cena konzumace za stůl č. 15 - úkol č. 3
+
+        manager.printPricePerTable(15);
+
+        //použití připravených metod - úkol č. 4
+
+        //Počet rozpracovaných objednávek
         System.out.println(manager.ordersInProgress(orders.getOrders()));
 
-        orders.orderFulfilled(orders.getOrders().get(1));
-        orders.orderFulfilled(orders.getOrders().get(2));
-        System.out.println(manager.averageOrderProcessingTime(orders.getOrders()));
+        //Seřazení objednávek dle času zadání
+        System.out.println(manager.sortOrdersByOrderTime(orders.getOrders()));
 
+        //Průměrná doba zpracování objednávek
+        System.out.println("Průměrná doba zpracování objednávek je: " + manager.averageOrderProcessingTime(orders.getOrders()) + " minut");
+
+        //Seznam jídel, která byla dnes objednána
+        System.out.println("Dnes byla objednána tato jídla: " + manager.dishesOrderedToday(orders.getOrders()));
+
+        //Seznam objednávek pro jeden stůl
+        manager.printOrdersByTable(15);
+
+        //Uložení informací do souboru - úkol č. 5
+
+        try {
+            menu.saveToFile("menu.txt");
+        } catch (OrdersException e) {
+            System.err.println("Nastala chyba při ukládání do souboru " + e.getLocalizedMessage());
+        }
+
+        try {
+            orders.saveToFile("orders.txt");
+        } catch (OrdersException e) {
+            System.err.println("Nastala chyba při ukládání do souboru " + e.getLocalizedMessage());
+        }
     }
     }
--- src/RestaurantManager.java
+++ src/RestaurantManager.java
@@ -1,20 +1,24 @@
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
 public class RestaurantManager {
 
-    List<Dish> menu;
-    List<Orders> orders;
 
-    public RestaurantManager(Cookbook cookbook, Orders orders) {
-        this.menu = cookbook.getCookbook();
+    private List<Orders> orders;
+
+    private List<Orders> ordersByTable;
+
+    public RestaurantManager(Orders orders) {
         this.orders = orders.getOrders();
     }
 
     public int ordersInProgress(List<Orders> orders) {
         int inProgress = 0;
-        for(Orders order : orders) {
+        for (Orders order : orders) {
             if (!order.isFulfilled()) {
                 inProgress++;
             }
@@ -25,18 +29,18 @@ public class RestaurantManager {
     public List<Orders> sortOrdersByOrderTime(List<Orders> orders) {
 
         orders.sort(
-        new Comparator<Orders>() {
-            @Override
-            public int compare(Orders o1, Orders o2) {
-                if (o1.getOrderTime().compareTo(o2.getOrderTime()) >= 0) {
-                return 1;
-                } else return -1;
-            }
-        });
+                new Comparator<Orders>() {
+                    @Override
+                    public int compare(Orders o1, Orders o2) {
+                        if (o1.getOrderTime().compareTo(o2.getOrderTime()) >= 0) {
+                            return 1;
+                        } else return -1;
+                    }
+                });
         return orders;
     }
 
-    public int averageOrderProcessingTime (List<Orders> orders) {
+    public int averageOrderProcessingTime(List<Orders> orders) {
         int numberOfOrders = 0;
         int totalProcessingTime = 0;
         for (Orders order : orders) {
@@ -45,6 +49,60 @@ public class RestaurantManager {
                 totalProcessingTime += ChronoUnit.MINUTES.between(order.getOrderTime(), order.getFulfilmentTime());
             }
         }
-        return totalProcessingTime /numberOfOrders;
+        return totalProcessingTime / numberOfOrders;
     }
-}
+
+    public List<Dish> dishesOrderedToday(List<Orders> orders) {
+        List<Dish> dishesOrderedToday = new ArrayList<>();
+        LocalDate today = LocalDate.now();
+        for (Orders order : orders) {
+            if (order.getOrderTime().toLocalDate().equals(today) && !dishesOrderedToday.contains(order.getMeal())) {
+                dishesOrderedToday.add(order.getMeal());
+            }
+        }
+        return dishesOrderedToday;
+    }
+
+    public List<Orders> exportOrdersByTable(int table) {
+        List<Orders> ordersByTable = new ArrayList<>();
+        for (Orders order : orders) {
+            if (order.getTable() == table) {
+                ordersByTable.add(order);
+            }
+        }
+        this.ordersByTable=ordersByTable;
+        return ordersByTable;
+        }
+
+
+        public void printOrdersByTable(int table) {
+            exportOrdersByTable(table);
+            String tablePadded = String.format("%02d", table);
+            System.out.println("** Objednávky pro stůl č. " + tablePadded + " **\n" + "****");
+            for (int i=0; i < ordersByTable.size(); i++) {
+                System.out.println(
+                        i+1 + ". "
+                        + ordersByTable.get(i).getMeal().getTitle() + " "
+                        + ordersByTable.get(i).getPieces() + "x "
+                        + "(" + ordersByTable.get(i).getMeal().getPrice().multiply(BigDecimal.valueOf(ordersByTable.get(i).getPieces())) + " Kč):\t"
+                        + ordersByTable.get(i).getOrderTime().getHour() + ":" + ordersByTable.get(i).getOrderTime().getMinute()
+                        + "-" + ordersByTable.get(i).getFulfilmentTime().getHour() + ":" + ordersByTable.get(i).getFulfilmentTime().getMinute()
+                        + "\t" + (ordersByTable.get(i).isPaid()? "zaplaceno" : ""));
+            }
+        }
+
+        public BigDecimal pricePerTable(int table){
+        BigDecimal pricePerTable = BigDecimal.valueOf(0);
+        for(Orders order : orders) {
+            if (order.getTable() == table) {
+                pricePerTable= pricePerTable.add(order.getMeal().getPrice().multiply(BigDecimal.valueOf(order.getPieces())));
+            }
+        }
+        return pricePerTable;
+        }
+
+        public void printPricePerTable(int table){
+        System.out.println("Celková cena konzumace pro stůl č. " + table + " je: " + pricePerTable(table) + " Kč");
+        }
+
+    }
